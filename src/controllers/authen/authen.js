1435e9091504e6465c03a261d3bdcb8e119773a3
nawaphon60 <nawaphonwichaidit@users.noreply.github.com>
2021-01-25 15:46:41 +0700

    update format

--- src/controllers/authen/authen.js
+++ src/controllers/authen/authen.js
@@ -1,31 +1,31 @@
 const Users = require('./../../models/users')
 const Utils = require('./../../utils')
 
-class AuthenController{
+class AuthenController {
 
-    constructor(){
+    constructor() {
 
     }
 
-    async login(req, res){
+    async login(req, res) {
         try {
             let input = req.body
             input.email = input.email || ""
 
-            if(!new Utils().validateEmail(input.email)){
+            if (!new Utils().validateEmail(input.email)) {
                 throw new Error("Invalid email.")
             }
 
-            if(!input.password){
+            if (!input.password) {
                 throw new Error("Require password.")
             }
 
             let password = new Utils().encryptPassword(input.password)
-            
+
             // check 
             let user = await Users.where('email', input.email).where('password', password).fetch()
-            
-            if(!user){
+
+            if (!user) {
                 throw new Error("Invalid email/password.")
             }
             user = user.toJSON()
@@ -41,7 +41,7 @@ class AuthenController{
                 token: token
             })
 
-        }catch(err){
+        } catch (err) {
             console.log(err.stack)
             res.status(400).json({
                 message: err.message
--- src/db.js
+++ src/db.js
@@ -7,23 +7,9 @@ const knex = require('knex')({
       password : process.env.DB_PASSWORD,
       database : process.env.DB_NAME,
       charset  : 'utf8mb4'
-    },
-    pool: {
-      min: 1,
-      max: 10,
-      // afterCreate: function(conn,done){
-      //   console.log('12122')
-      //   conn.query('SELECT 1;', function (err) {
-      //     if(err){
-      //       console.log('DB ERROR',err)
-      //     }else{
-      //       console.log('CONNECT SUCCESS.')
-      //     }
-      //     done(conn, err)
-      //   })
-      // }
     }
   })
+  
   const bookshelf = require('bookshelf')(knex)
 
   module.exports = bookshelf
